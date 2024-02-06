c1eff8f97933d47499f9420352029d732c2a9a37
mascott82 <m.a.scott@live.com>
2024-02-06 14:20:55 -0600

    Featuring - Implement the function of search.

--- backend/routes/search.js
+++ backend/routes/search.js
@@ -5,13 +5,43 @@
  * See: https://expressjs.com/en/guide/using-middleware.html#middleware.router
  */
 
-const express = require('express');
-const router  = express.Router();
+const express = require('express')
+const router  = express.Router()
+const axios = require('axios')
 
 router.get('/:i', (req, res) => {
-  const ingredients = req.params.i;
+  const params = req.params.i
 
-  console.log("====", ingredients);
-});
+  const ingredients = params.split(",")
 
-module.exports = router;
+  const apiEndpoint = 'https://api.spoonacular.com/recipes/findByIngredients'
+
+  const options = {
+    params: {
+      ingredients: 'apples,flour,sugar',
+      number: '5',
+      ignorePantry: 'true',
+      ranking: '1'
+    },
+    headers: {
+      'Content-Type': 'application/json',
+      'x-api-key':  'd244d7df0bca4e509d34d9496190e714'
+    }
+  }
+
+  axios
+    .get(apiEndpoint, options)
+    .then((response) => {
+      console.log(response.data)
+    })
+    .catch((err) => {
+      console.err("====", err)
+    })
+    .finally(() => {
+      console.log("--End--")
+    })
+
+  res.json(ingredients)
+})
+
+module.exports = router
