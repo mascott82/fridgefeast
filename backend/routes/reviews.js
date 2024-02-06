3b36b05a9b8e013f1e79f31c6458f80028716bc4
mascott82 <m.a.scott@live.com>
2024-02-06 15:06:13 -0600

    Featuring - Implementing the router for recipe.

--- backend/routes/reviews.js
+++ backend/routes/reviews.js
@@ -8,13 +8,47 @@
 const express = require('express');
 const router  = express.Router();
 
-const reviews = require('../db/queries/review');
+const { getReviews, addReview } = require('../db/queries/review');
 
-router.get('/list', (req, res) => {
+router.get('/', (req, res) => {
   const toUserId = req.session.userId ? req.session.userId : 1;
   const toUserName = req.session.username ? req.session.username : '';
-  
-  console.log("===", reviews);
+  const results = []
+  getReviews()
+    .then((reviews) => {
+      reviews.forEach(review => {
+        results.push(review)
+      })
+
+      res.json(results)
+    })
 });
 
-module.exports = router;
+router.get('/new', (req, res) => {
+  res.render('review')
+})
+
+router.post('/save', (req, res) => {
+  const rating = req.body.rating
+  const userId = req.body.userId
+  const userEmail = req.body.userEmail
+  const comment = req.body.comment
+  const recipeId = req.body.recipeId
+
+  const review = {
+    user_id:  userId,
+    recipe_id:  recipeId,
+    rating: rating,
+    comment:  comment
+  }
+
+  addReview(review)
+    .then(() => {
+      res.send({ message: 1 })
+    })
+    .catch((error) => {
+      console.error("Error marking the review. ", error)
+    })
+})
+
+module.exports = router
