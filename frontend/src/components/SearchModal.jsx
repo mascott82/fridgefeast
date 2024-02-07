302f93834e033c3587ee077a2e83b77ad61e5109
Victoria Wong <victoria.ys.wong@gmail.com>
2024-02-07 00:50:20 -0500

    update styling

--- frontend/src/components/SearchModal.jsx
+++ frontend/src/components/SearchModal.jsx
@@ -7,26 +7,24 @@ function SearchModal({onClose}) {
   return (
     <Modal show={true} onHide={onClose}>
       <Modal.Header closeButton>
-        <Modal.Title>Search Modal</Modal.Title>
+        <Modal.Title>Find Recipes</Modal.Title>
       </Modal.Header>
       <Modal.Body>
         <Card>
+          <Card.Title>Add ingredients (up to 5)</Card.Title>
           <Card.Body>
             <input
               type="text"
-              placeholder="Input Ingredients (up to 5)"
+              placeholder="Type an ingredient name..."
               className="form-control mb-2"
             />
-            {/* Your search results can be displayed here */}
+            {/* Search items */}
           </Card.Body>
         </Card>
       </Modal.Body>
       <Modal.Footer>
-        <Button variant="secondary" onClick={onClose}>
-          Close
-        </Button>
         <Button variant="primary" onClick={onClose}>
-          Save Changes
+          Search Recipes
         </Button>
       </Modal.Footer>
     </Modal>
