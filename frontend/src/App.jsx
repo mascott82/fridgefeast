e3e1d44c894cfa0609e5e3f6173845feb3018625
Victoria Wong <victoria.ys.wong@gmail.com>
2024-02-06 20:42:22 -0500

    merge conflicts

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,7 @@
-
-import { useState } from 'react'
-import './App.css'
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Signup from "./pages/Signup";
+import {useState} from "react"
+import "./App.css"
+import {BrowserRouter, Routes, Route} from "react-router-dom"
+import Signup from "./pages/Signup"
 // import {useState} from "react"
 import reactLogo from "./assets/react.svg"
 import viteLogo from "/vite.svg"
@@ -11,23 +10,18 @@ import NavigationBar from "./components/NavigationBar"
 import Homepage from "./components/Homepage"
 import "./styles/Homepage.css"
 
-
 function App() {
   // const [count, setCount] = useState(0)
 
   return (
-  <>
-  <NavigationBar />
-  <BrowserRouter>
-      <Routes>
-        <Route path="/signup" element={<Signup />} />
-      </Routes>
-  </BrowserRouter>
-  </>
-    )
     <>
       <NavigationBar />
       <Homepage />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/signup" element={<Signup />} />
+        </Routes>
+      </BrowserRouter>
     </>
   )
 }
