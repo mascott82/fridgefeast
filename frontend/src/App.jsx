dd572ece25f1ded0dc10853d340528e71e94a9e2
yoonjookim <yjblossom@gmail.com>
2024-02-07 00:53:55 -0500

    Feature:Login page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,6 +3,7 @@ import "./App.css"
 import {BrowserRouter, Routes, Route} from "react-router-dom"
 import Signup from "./pages/Signup"
 import Login from "./pages/Login"
+import ProtectedRoute from "./pages/ProtectedRoute"
 // import {useState} from "react"
 import reactLogo from "./assets/react.svg"
 import viteLogo from "/vite.svg"
@@ -11,14 +12,20 @@ import NavigationBar from "./components/NavigationBar"
 import Homepage from "./components/Homepage"
 import "./styles/Homepage.css"
 import { CookiesProvider, useCookies } from "react-cookie";
+import { useNavigate } from 'react-router-dom';
+
+const RedirectTo =() =>{
+  const navigate = useNavigate();
+  navigate('/');
+  return (<></>);
+}
 
 function App() {
-  
   const [cookies, setCookie] = useCookies(["user"]);
 
-  function handleLogin(user) {
-    setCookie("user", user, { path: "/" });
-    console.log("user after login", user)
+  function handleLogin(emailAndAuthToken) {
+    setCookie("user", emailAndAuthToken, { path: "/" });
+    console.log("After login: emailAndAuthToken = ", emailAndAuthToken)
   }
   
 
@@ -30,7 +37,9 @@ function App() {
         <Routes>
           <Route path="/" element={<Homepage />} /> 
           <Route path="/signup" element={<Signup />} />
-          <Route path="/login" element={<Login onLogin={handleLogin}/>} />
+          <Route path="/login" element={cookies.user == null ? <Login onLogin={handleLogin}/> : <RedirectTo />} />
+          <Route element={<ProtectedRoute currentUser={cookies.user}/>}>
+          </Route>
         </Routes>
       </BrowserRouter>
       </CookiesProvider>
