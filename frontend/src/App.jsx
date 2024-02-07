dfdfd5d923c1fb20175fa077fd40344aff2eacba
yoonjookim <yjblossom@gmail.com>
2024-02-06 22:01:54 -0500

    Feature: Implement login page/ session cookie/ WIP

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,6 +2,7 @@ import {useState} from "react"
 import "./App.css"
 import {BrowserRouter, Routes, Route} from "react-router-dom"
 import Signup from "./pages/Signup"
+import Login from "./pages/Login"
 // import {useState} from "react"
 import reactLogo from "./assets/react.svg"
 import viteLogo from "/vite.svg"
@@ -9,19 +10,30 @@ import "./App.css"
 import NavigationBar from "./components/NavigationBar"
 import Homepage from "./components/Homepage"
 import "./styles/Homepage.css"
+import { CookiesProvider, useCookies } from "react-cookie";
 
 function App() {
-  // const [count, setCount] = useState(0)
+  
+  const [cookies, setCookie] = useCookies(["user"]);
+
+  function handleLogin(user) {
+    setCookie("user", user, { path: "/" });
+    console.log("user after login", user)
+  }
+  
 
   return (
     <>
       <NavigationBar />
-      <Homepage />
+      <CookiesProvider>
       <BrowserRouter>
         <Routes>
+          <Route path="/" element={<Homepage />} /> 
           <Route path="/signup" element={<Signup />} />
+          <Route path="/login" element={<Login onLogin={handleLogin}/>} />
         </Routes>
       </BrowserRouter>
+      </CookiesProvider>
     </>
   )
 }
