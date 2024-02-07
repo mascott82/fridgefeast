a6a5cd0cb8c3a810a4f747ce1e8b6f13828845e5
yoonjookim <yjblossom@gmail.com>
2024-02-07 01:27:33 -0500

    Feature:Login redirect issue resolve after login

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,4 @@
-import {BrowserRouter, Routes, Route} from "react-router-dom"
+import {BrowserRouter, Routes, Route, useNavigate} from "react-router-dom"
 import Signup from "./routes/Signup"
 import Login from "./routes/Login"
 import ProtectedRoute from "./routes/ProtectedRoute"
@@ -7,12 +7,11 @@ import {CookiesProvider, useCookies} from "react-cookie"
 import NavigationBar from "./components/NavigationBar"
 import Homepage from "./components/Homepage"
 import "./styles/Homepage.css"
-import { useNavigate } from 'react-router-dom';
 import "./styles/NavigationBar.css"
 
 const RedirectTo =() =>{
   const navigate = useNavigate();
-  navigate('/');
+  setTimeout(() =>{navigate('/')}, 100);
   return (<></>);
 }
 
@@ -32,7 +31,7 @@ function App() {
         <Routes>
           <Route path="/" element={<Homepage />} /> 
           <Route path="/signup" element={<Signup />} />
-          <Route path="/login" element={cookies.user == null ? <Login onLogin={handleLogin}/> : <RedirectTo />} />
+          <Route path="/login" element={cookies.user == null ? <Login onLogin={handleLogin}/> : <RedirectTo/>} />
           <Route element={<ProtectedRoute currentUser={cookies.user}/>}>
           </Route>
         </Routes>
