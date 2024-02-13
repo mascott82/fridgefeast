657061953f961f79fac0a4e26492246997f48e2c
mascott82 <m.a.scott@live.com>
2024-02-13 07:54:51 -0600

    Bugfixing - Fix the issue of page reloading for fav button.

--- frontend/src/components/FavouriteButton.jsx
+++ frontend/src/components/FavouriteButton.jsx
@@ -1,24 +1,31 @@
+import { useState } from 'react'
 import { FaHeart, FaRegHeart } from 'react-icons/fa';
 import axios from 'axios';
 const backend_endpoint_user_fav_delete = 'http://0.0.0.0:3000/fav/delete'
 const backend_endpoint_user_fav_add = 'http://0.0.0.0:3000/fav/add'
 
 const FavouriteButton = ({ addNew, userid, recipeid}) => {
+  
+  const [isFavorited, setIsFavorited] = useState(addNew);
+
+  console.log(`== ${addNew} ${userid} ${recipeid} ==`)
   const clickAction =() => {
     const userRecipeTarget = { userid: userid, recipeid: recipeid };
-    const url = (addNew ? backend_endpoint_user_fav_add : backend_endpoint_user_fav_delete)
+
+    const url = (isFavorited ? backend_endpoint_user_fav_add : backend_endpoint_user_fav_delete)
     axios.post(url, userRecipeTarget).then((response) => {
       const qty = response.data.qty;
       console.log("change-fav-qty", qty)
+      setIsFavorited(!isFavorited);
     }).catch((error) => {
       console.error('Error fav recipe query:', error);
     })
-    window.location.reload();
   }
+
   return (
-    <button onClick={clickAction} style={{ backgroundColor: 'white' }}>
-      {addNew ?  <FaRegHeart /> : <FaHeart color="red" /> }
-    </button>
+    <div>
+      {isFavorited ?  <FaRegHeart onClick={clickAction}/> : <FaHeart color="red" onClick={clickAction}/> }
+    </div>
   );
 };
 
