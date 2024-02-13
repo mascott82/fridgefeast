c0325aa9608f63c57b2948f8e7ee4c261c0ff36c
mascott82 <m.a.scott@live.com>
2024-02-13 08:59:28 -0600

    Bugfixing - Version rollback for the fav function.

--- frontend/src/components/FavouriteButton.jsx
+++ frontend/src/components/FavouriteButton.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+// import { useState } from 'react'
 import { FaHeart, FaRegHeart } from 'react-icons/fa';
 import axios from 'axios';
 const backend_endpoint_user_fav_delete = 'http://0.0.0.0:3000/fav/delete'
@@ -6,26 +6,30 @@ const backend_endpoint_user_fav_add = 'http://0.0.0.0:3000/fav/add'
 
 const FavouriteButton = ({ addNew, userid, recipeid}) => {
   
-  const [isFavorited, setIsFavorited] = useState(addNew);
+  // const [isFavorited, setIsFavorited] = useState(addNew);
 
-  console.log(`== ${addNew} ${userid} ${recipeid} ==`)
   const clickAction =() => {
     const userRecipeTarget = { userid: userid, recipeid: recipeid };
 
-    const url = (isFavorited ? backend_endpoint_user_fav_add : backend_endpoint_user_fav_delete)
+    // const url = (isFavorited ? backend_endpoint_user_fav_add : backend_endpoint_user_fav_delete)
+    const url = (addNew ? backend_endpoint_user_fav_add : backend_endpoint_user_fav_delete)
     axios.post(url, userRecipeTarget).then((response) => {
       const qty = response.data.qty;
       console.log("change-fav-qty", qty)
-      setIsFavorited(!isFavorited);
+      // setIsFavorited(!isFavorited);
+      window.location.reload()
     }).catch((error) => {
       console.error('Error fav recipe query:', error);
     })
   }
 
   return (
-    <div>
-      {isFavorited ?  <FaRegHeart onClick={clickAction}/> : <FaHeart color="red" onClick={clickAction}/> }
-    </div>
+    // <div>
+    //   {isFavorited ?  <FaRegHeart onClick={clickAction}/> : <FaHeart color="red" onClick={clickAction}/> }
+    // </div>
+    <button onClick={clickAction} style={{ backgroundColor: 'white' }}>
+      {addNew ?  <FaRegHeart /> : <FaHeart color="red" /> }
+    </button>
   );
 };
 
